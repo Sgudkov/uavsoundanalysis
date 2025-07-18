3a6a0906be5a064e83b2dc7570dfe9a02a67cca3
Sergey <sgudkovv@gmail.com>
2025-07-18 10:28:35 +0300

    Added login/logout Refactor for base.html

--- uavsoundanalysis/uavmanage/settings.py
+++ uavsoundanalysis/uavmanage/settings.py
@@ -134,3 +134,6 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # STATIC_ROOT = os.path.join(BASE_DIR / "static", 'staticfiles')
+
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
